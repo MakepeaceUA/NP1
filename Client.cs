aa0eee532313aeeb258acf6f15cac170987bed27
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2025-02-17 22:43:56 +0100

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp25
+{
+    class UDPClient
+    {
+        static void Main()
+        {
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 12345);
+
+            string message = "Привет, сервер!";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            udpClient.Send(data, data.Length, serverEP);
+            Console.WriteLine("Сообщение отправлено на серверве...");
+
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            byte[] receiveData = udpClient.Receive(ref remoteEP);
+            string response = Encoding.UTF8.GetString(receiveData);
+            Console.WriteLine($"Ответ сервера: {response}");
+
+            udpClient.Close();
+        }
+    }
+}
+
+
+
+
+
---
+++ Server.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp24
+{
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
+    class UDPServer
+    {
+        static void Main()
+        {
+            UdpClient udpServer = new UdpClient(12345);
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("Сервер запущен,ожидание сообщений...");
+
+            while (true)
+            {
+                byte[] receiveData = udpServer.Receive(ref remoteEP);
+                string receivedMessage = Encoding.UTF8.GetString(receiveData);
+                Console.WriteLine($"Получено сообщение от клиента {remoteEP}: {receivedMessage}");
+
+                string response = "Привет, клиент!";
+                byte[] responseData = Encoding.UTF8.GetBytes(response);
+                udpServer.Send(responseData, responseData.Length, remoteEP);
+            }
+        }
+    }
+}
